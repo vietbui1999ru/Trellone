f881866dea159406312f8c9380db9df2d686d15c
vietbui1999ru <buiquocviet99@gmail.com>
2022-09-03 19:45:22 -0500

    commit the splash screen, intro screen, and gitignore for the project

---
+++ app/src/main/java/com/projemanag/IntroActivity.kt
@@ -0,0 +1,21 @@
+package com.projemanag
+
+import android.content.Intent
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        // END
+    }
+}
---
+++ app/src/main/java/com/projemanag/SplashActivity.kt
@@ -0,0 +1,44 @@
+package com.projemanag
+
+import android.content.Intent
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_splash.*
+
+// TODO (Step 1: Add new activity named as splash activity.)
+// START
+const val delayToMain : Long = 2500
+class SplashActivity : AppCompatActivity() {
+
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
+        super.onCreate(savedInstanceState)
+        // This is used to align the xml view to this class
+        setContentView(R.layout.activity_splash)
+
+        // TODO (Step 6: Add the full screen flags here.)
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
+        // START
+        // END
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        // END
+
+        val typeface: Typeface =
+            Typeface.createFromAsset(assets, "Cookie-Regular.ttf")
+        tv_app_name.typeface = typeface
+
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+        }, delayToMain)
+    }
+}
+// END
